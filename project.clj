98ff908de82c0df82471af68f213fdb59bafcdc5
Baptiste Dupuch <baptiste.dupuch@gmail.com>
2017-10-17 17:36:34 +0200

    - clojar setup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kibu/pushy "0.3.8"
+(defproject dupuchba/pushy "0.3.8"
   :description "HTML5 pushState for Clojurescript"
   :url "https://github.com/kibu-australia/pushy"
   :license {:name "Eclipse Public License"
